5a9ff8c75a5858eecbf36a3cd33f2e491730e9b1
Toan <146208081+VanToanUNIVERSE@users.noreply.github.com>
2026-08-31 20:56:09 +0700

    feat(models): add Category, Subcategory, Product, ProductImage, Order, OrderItem,

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'price',
+        'hot',
+        'description',
+        'size',
+        'quantity',
+        'subcategory_id',
+        'active',
+    ];
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
